41de9cd302046fe823a027c3734142c257e9b62b
Oksana_Melnyk <kc.19870625@gmail.com>
2025-10-30 01:32:04 +0200

    update file app

--- app/Loading.tsx
+++ app/Loading.tsx
@@ -1,3 +1,7 @@
-export default function NotesLoading() {
-  return <p>Loading, please wait...</p>;
+import React from 'react';
+
+export default function Loading() {
+  return (
+    <p>Loading, please wait...</p>
+  );
 }
