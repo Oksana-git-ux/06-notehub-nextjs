54476b50f73bebf7394077b3b6a3436b73d9f8c0
Oksana_Melnyk <kc.19870625@gmail.com>
2025-11-03 09:47:37 +0200

    apdate file app/layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           <main>{children}</main>
           <Footer />
         </TanStackProvider>
+        <div id="modal-root" />
       </body>
     </html>
   );
